6b40e382faebb675ac996819e731f2be9d33b66e
Aditya123621 <adityarox621@example.com>
2025-08-05 13:02:31 +0530

    fix: fixed google authentication

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,8 +14,11 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
